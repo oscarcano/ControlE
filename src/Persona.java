c336b46a8d6dcbb5521a546765c27dbb326e8cb0
Oscar Cano <oscarcano.net@gmail.com>
2014-01-07 16:57:57 -0700

    commit segundo

--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,6 @@
+
+import java.util.Date;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,19 @@
  * @author oscar
  */
 public class Persona {
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private Date fechaNacimiento;
+
+    public Persona(String nombre, String primerApellido, String segundoApellido, Date fechaNacimiento) {
+        this.nombre = nombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
     
+    
+    
+           
 }
